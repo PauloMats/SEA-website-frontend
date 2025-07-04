81de1ae20131399098304186b4e9c4d0b38b01d1
pm.mats98 <pm.mats98@gmail.com>
2025-07-04 12:05:16 +0000

    feat: styles

--- sea-website/src/app/layout.tsx
+++ sea-website/src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
         <title>SEA Platform</title>
       </head>
       <body>
